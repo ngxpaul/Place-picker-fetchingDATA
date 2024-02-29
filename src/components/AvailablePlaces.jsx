24c663b9899e3379c134cf68b1e25c57587916d0
ngxpaul <phongnx.22git@vku.udn.vn>
2024-02-29 10:03:52 +0700

    update Loading text when fetching Data

--- src/components/AvailablePlaces.jsx
+++ src/components/AvailablePlaces.jsx
@@ -1,12 +1,16 @@
 import Places from "./Places.jsx";
 import { useEffect, useState } from "react";
 export default function AvailablePlaces({ onSelectPlace }) {
+  const [isFetching,setIsFetching] = useState(false);
   const [availablePlaces, setAvailablePlaces] = useState([]);
   useEffect(() => {
     async function fetchPlaces() {
+      setIsFetching(true);
       const response = await fetch('http://localhost:3000/places');
       const resData = await response.json();
       setAvailablePlaces(resData.places);
+      setIsFetching(false);
+
     }
     fetchPlaces();
     // fetch("http://localhost:3000/places")
@@ -23,6 +27,8 @@ export default function AvailablePlaces({ onSelectPlace }) {
     <Places
       title="Available Places"
       places={availablePlaces}
+      isLoading={isFetching}
+      loadingText="Fetching place Data..."
       fallbackText="No places available."
       onSelectPlace={onSelectPlace}
     />
