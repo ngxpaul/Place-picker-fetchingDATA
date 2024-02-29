64e62eed7e959db6690d19a1a6631f742a2b7387
ngxpaul <phongnx.22git@vku.udn.vn>
2024-02-29 10:52:47 +0700

    extract code && split fetch data to new component function http.js

--- src/components/AvailablePlaces.jsx
+++ src/components/AvailablePlaces.jsx
@@ -2,6 +2,7 @@ import Places from "./Places.jsx";
 import { useEffect, useState } from "react";
 import Error from "./Error";
 import { sortPlacesByDistance } from "../loc.js";
+import  { fetchAvailablePlaces } from "../http.js";
 export default function AvailablePlaces({ onSelectPlace }) {
   const [isFetching, setIsFetching] = useState(false);
   const [availablePlaces, setAvailablePlaces] = useState([]);
@@ -10,15 +11,10 @@ export default function AvailablePlaces({ onSelectPlace }) {
     async function fetchPlaces() {
       setIsFetching(true);
       try {
-        const response = await fetch("http://localhost:3000/places");
-        const resData = await response.json();
-
-        if (!response.ok) {
-          throw new Error("Failed to fetch places");
-        }
+        const places = await fetchAvailablePlaces();
         navigator.geolocation.getCurrentPosition((position) => {
           const sortedPlaces = sortPlacesByDistance(
-            resData.places,
+            places,
             position.coords.latitude,
             position.coords.longitude
           );
